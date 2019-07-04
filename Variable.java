408adbb3088e59958fd9734ee6c182bcf3143a63
Wenister <51784755+Wenister@users.noreply.github.com>
2019-07-04 23:03:33 +0300

    Update Variable.java

--- Variable.java
+++ Variable.java
@@ -1,13 +1,14 @@
 public class Variable {
-	public static void main(String args[]){
-	byte windows =10;
-	short systemType =64;
-	int numberOfCores = 4210;
-	long cpu =4;
-	float cpuFrequency = 2.90F;
-	double memory =8.00;
-	char intel ='R';
-	boolean windowsActivation=true;
+	public static void main(String[] args){
+	 byte windows = 10;
+	 short systemType = 64;
+	 int numberOfCores = 4210;
+	 long cpu = 4;
+	 float cpuFrequency = 2.90F;
+	 double memory = 8.00;
+	 char intel = 'R';
+	 boolean windowsActivation = true;
+		
 	System.out.println(" Выпуск Windows "+ windows);
 	System.out.println(" Тип системы " + systemType);
 	System.out.println(" Номер процессора" + numberOfCores);
@@ -16,7 +17,5 @@ public class Variable {
 	System.out.println(" Установленая память " + memory);
 	System.out.println(" Процессор " + intel);
 	System.out.println(" Активация Windows " + windowsActivation);
-
-
 	}
 }
