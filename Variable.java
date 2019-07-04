e433dfa619687d9e35f6747cb93efead038e8b60
Wenister <51784755+Wenister@users.noreply.github.com>
2019-07-04 20:30:05 +0300

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,22 @@
+public class Variable {
+	public static void main(String args[]){
+	byte windows =10;
+	short systemType =64;
+	int numberOfCores = 4210;
+	long cpu =4;
+	float cpuFrequency = 2.90F;
+	double memory =8.00;
+	char intel ='R';
+	boolean windowsActivation=true;
+	System.out.println(" Выпуск Windows "+ windows);
+	System.out.println(" Тип системы " + systemType);
+	System.out.println(" Номер процессора" + numberOfCores);
+	System.out.println(" Количество процессоров " + cpu);
+	System.out.println(" Частота процессора " + cpuFrequency);
+	System.out.println(" Установленая память " + memory);
+	System.out.println(" Процессор " + intel);
+	System.out.println(" Активация Windows " + windowsActivation);
+
+
+	}
+}
