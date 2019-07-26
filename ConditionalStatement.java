143080c0cd261aa3b11760e2fc77f816aa6d89a9
Wenister <51784755+Wenister@users.noreply.github.com>
2019-07-26 15:32:59 +0300

    Update ConditionalStatement.java

--- ConditionalStatement.java
+++ ConditionalStatement.java
@@ -1,10 +1,7 @@
-import java.util.Scanner;
-
 public class ConditionalStatement {
 	public static void main(String[] args) {
 		int age = 27;
-		boolean men = true;
-		boolean women = true;
+		boolean isMan = true;
 		double growth = 1.86;
 		char name = 'I';
 
@@ -12,29 +9,26 @@ public class ConditionalStatement {
 			System.out.println(" Я взрослый ");
 		}
 
-		if (men==true) {
+		if (isMan) {
 			System.out.println(" Я мужчина ");
 		}
 
-		if (women==true) {
+		if (!isMan==true) {
 			System.out.println(" Я женщина ");
 		}
 
 		if (growth < 1.80) {
 			System.out.println(" Не такой уж я и выскокий ");
-
 		} else {
 			System.out.println(" Можно подумать и о волейболе ");
 		}
 
-		if ( name =='M') {
+		if (name== 'M') {
 			System.out.println(" I'm Mihail ");
-		} else if ( name =='I') {
+		} else if (name== 'I') {
 			System.out.println(" I'm Ingrid ");
 		} else {
 			System.out.println(" Your name is strange ");
 		}		
-	}
-	
-	
+	}	
 }
